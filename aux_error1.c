1f6691ff61e6d356247bcf97f1fffb5cc69ca274
bibest <bibhestee@ggmail.com>
2022-06-16 14:05:30 -0700

    Aux_error Completed

--- aux_error1.c
+++ aux_error1.c
@@ -2,11 +2,11 @@
 
 /**
  * strcat_cd - function that concatenates the message for cd error
- *
  * @datash: data relevant (directory)
  * @msg: message to print
  * @error: output message
  * @ver_str: counter lines
+ *
  * Return: error message
  */
 char *strcat_cd(data_shell *datash, char *msg, char *error, char *ver_str)
